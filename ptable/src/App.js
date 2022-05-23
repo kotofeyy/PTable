878680fc087ebb41340437439048943cd09f8b5d
kotofeyy <58441304+kotofeyy@users.noreply.github.com>
2022-05-24 01:15:18 +0300

    3d here

--- ptable/src/App.js
+++ ptable/src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useRef, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+
+function Box(props) {
+  const mesh = useRef();
+
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+  useFrame((state, delta) => (mesh.current.rotation.x += 0.01));
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}
+      rotation={[Math.PI / 2, 5, 2]}
+    >
+      <sphereGeometry />
+
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Box position={[-1.2, 0, 0]} />
+        <Box position={[1.2, 0, 0]} />
+        <Box position={[2.3, 0, 0]} />
+        <Box position={[3.4, 0, 0]} />
+        <Box position={[7.5, 3, 0]} />
+      </Canvas>
     </div>
   );
 }
